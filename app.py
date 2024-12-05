2bd07415f6b2d0a0703e018fa7b520dfbc4ab47a
Murahari <mmaddi@clarku.edu>
2024-12-05 09:07:08 +0530

    Deployment in render

--- app.py
+++ app.py
@@ -134,3 +134,5 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Create the tables (ensure it only runs once)
     app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
