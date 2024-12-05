2e04ca89ead5dd2ceba4c7d79a0f3253a0044723
Murahari <mmaddi@clarku.edu>
2024-12-05 09:57:42 +0530

    Deployment in render

--- app.py
+++ app.py
@@ -134,6 +134,5 @@ def send_chat_history(user_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Create the tables (ensure it only runs once)
-    app.run(debug=True)
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
